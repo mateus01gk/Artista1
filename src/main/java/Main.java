80ada42993282cfa78b8fdacc2d2c85872f56774
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-05-14 00:30:59 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+      public static void main(String[] args) {
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        System.out.println("***************");
+        System.out.println("*Filme : Rio 2*");
+        System.out.println("***************");
+        System.out.println(" ");
+        System.out.println("Lançamento do Filme Rio 2");
+        System.out.println("27 de março de 2014");
+        System.out.println(" ");
+        System.out.println("Sinopse ");
+
+        System.out.println("======================================================");
+
+        System.out.println("As araras Blu e Jade vivem felizes com seus filhos no Rio de Janeiro. Quando seus donos, Túlio e Linda, encontram pássaros de sua espécie na Amazônia, eles decidem partir para novas aventuras na região Norte do país. Só que nem tudo é perfeito: Nigel, o velho inimigo de Blu e Jade, está de volta para se vingar.");
+        System.out.println("====================================================== ");
+        System.out.println(" ");
+        System.out.println("+++++++++++++++++++++++");
+        System.out.println("+ Endereço do Trailer +");
+        System.out.println("+++++++++++++++++++++++");
+        System.out.println("  ");
+
+        System.out.println("https://www.youtube.com/watch?v=_QNrbbGpLrc");
+        System.out.println(" ");
+        System.out.println("Opinião: Divertido e Alegre ");
+        System.out.println(" ");
+
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+  }
+}
